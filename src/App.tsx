390551313b0db50f667c0bb33ecfe68a95c9314b
Amilkar Sanchez <amilkaralan@gmail.com>
2025-01-31 20:29:57 -0600

    App terminada

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
+  const [advice, setAdvice] = useState({
+    id: 0,
+    advice: ""
+  })
 
+  const fetchAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then(res => res.json())
+      .then(data => setAdvice({ id: data.slip.id, advice: data.slip.advice }))
+  }
 
+  useEffect(() => {
+    fetchAdvice()
+  }, []);
   return (
 
     <main className='w-screen h-screen flex justify-center items-center'>
-      <Card />
+      <Card getAdvice={fetchAdvice} advice={advice} />
     </main>
 
   )
